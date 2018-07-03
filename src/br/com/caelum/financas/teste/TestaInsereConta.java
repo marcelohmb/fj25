79a8da7f49b20f5ccf9bf471d435840389f42c23
marcelohmb <marcelo.hmb@gmail.com>
2018-07-02 23:01:24 -0300

    primeiraAula

---
+++ src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -0,0 +1,33 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TestaInsereConta {
+	
+	public static void main(String[] args) {
+		
+		long inicio = System.currentTimeMillis();
+		
+				
+		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		Conta conta = new Conta();
+		conta.setTitular("José Roberto");
+		conta.setBanco("Banco do Brasil");
+		conta.setNumero("123456-6");
+		conta.setAgencia("0999");
+		
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+		manager.close();
+				
+		System.out.println("Conta gravada com sucesso!");
+		
+		long fim = System.currentTimeMillis();
+		System.out.println("Executado em: " + (fim - inicio) + "ms");
+	}
+}
